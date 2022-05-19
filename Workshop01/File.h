07523382741e09ed3ea2dd1ae72645234d94b753
Vinh Nhan <vnhan1@myseneca.ca>
2022-05-19 10:38:54 -0400

    Add top comment (student info and declaration)

--- Workshop01/File.h
+++ Workshop01/File.h
@@ -1,3 +1,12 @@
+/*
+Full Name   : Vinh Nhan
+Seneca email: vnhan1@myseneca.ca
+Student ID  : 171452212
+Date        : May 18, 2022
+
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+
 #include "ShoppingRec.h"
 
 #ifndef sdds_File_H 
